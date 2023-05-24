87db0c1e8834c299492192ec72617b77f49677c9
LucasBywalec <lukasz.bywalec@outlook.com>
2023-05-24 10:48:27 +0200

    fixed application start

--- src/main/java/com/barbarian/barbarianfood/BarbarianfoodApplication.java
+++ src/main/java/com/barbarian/barbarianfood/BarbarianfoodApplication.java
@@ -3,7 +3,6 @@ package com.barbarian.barbarianfood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
--- src/main/java/com/barbarian/barbarianfood/controller/AuthController.java
+++ src/main/java/com/barbarian/barbarianfood/controller/AuthController.java
@@ -4,12 +4,13 @@ import com.barbarian.barbarianfood.service.AuthService;
 import com.zaiapi.openapi.api.AuthApi;
 import com.zaiapi.openapi.model.SignInRequest;
 import com.zaiapi.openapi.model.SignUpRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController implements AuthApi {
-
+    @Autowired
     AuthService authService;
 
     @Override
